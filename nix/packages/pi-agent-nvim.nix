6d3d735557e2ba8eb0571d61a9ff0ea222fef16d
Aldo Borrero <82811+aldoborrero@users.noreply.github.com>
2026-02-05 09:40:03 +0000

    feat: filter extra unnecessary directories when creating the plugin

--- nix/packages/pi-agent-nvim.nix
+++ nix/packages/pi-agent-nvim.nix
@@ -1,8 +1,26 @@
 { pkgs }:
+let
+  inherit (pkgs) lib;
+  src = lib.cleanSourceWith {
+    src = ../..;
+    filter =
+      path: type:
+      let
+        baseName = baseNameOf path;
+      in
+      !(
+        baseName == ".direnv"
+        || baseName == ".ai"
+        || baseName == ".git"
+        || baseName == "result"
+        || lib.hasSuffix ".qcow2" baseName
+      );
+  };
+in
 pkgs.vimUtils.buildVimPlugin {
   pname = "pi-agent-nvim";
   version = "0.1.0";
-  src = ../..;
+  inherit src;
   meta = {
     description = "Pi Agent Neovim plugin";
     homepage = "https://github.com/aldoborrero/pi-agent.nvim";
